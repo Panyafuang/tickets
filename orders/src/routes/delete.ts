e3888a21f6cffc5b143db1b7740aa13e89d7b08b
Thanadilok Panyafuang <panyafuang@Thanadiloks-Mac-mini.local>
2025-06-04 21:59:09 +0700

    update order-srv

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,23 @@
 import express, { Request, Response } from "express";
+import { Order } from "../models/order";
+import { NotAuthorizedError, NotFoundError, OrderStatus } from "@xtptickets/common";
 
 const router = express.Router();
 
 router.delete("/api/orders/:orderId", async (req: Request, res: Response) => {
-  res.send({});
+  const order = await Order.findById(req.params.orderId);
+
+  if (!order) {
+    throw new NotFoundError();
+  }
+  if (order.userId !== req.currentUser!.id) {
+    throw new NotAuthorizedError();
+  }
+
+  order.status = OrderStatus.Cancelled;
+  await order.save();
+
+  res.status(204).send(order);
 });
 
 export { router as deleteOrderRouter };
--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,9 +1,21 @@
 import express, { Request, Response } from "express";
+import { Order } from "../models/order";
+import { NotAuthorizedError, NotFoundError } from "@xtptickets/common";
 
 const router = express.Router();
 
 router.get("/api/orders/:orderId", async (req: Request, res: Response) => {
-  res.send({});
+  const order = await Order.findById(req.params.orderId).populate('ticket');
+
+  if (!order) {
+    throw new NotFoundError();
+  }
+  if (order.userId !== req.currentUser!.id) {
+    throw new NotAuthorizedError();
+  }
+
+
+  res.send(order);
 });
 
 export { router as showOrderRouter };
