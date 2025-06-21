34a9302833c295232a2d4414e81f213b6c98e1e0
BRKIDNEY\thanadilok.p <thanadilok.p@brkidney.org>
2025-06-21 16:14:28 +0700

    updated payments tests

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -4,6 +4,21 @@ import { app } from "../../app";
 import { Order } from "../../models/order";
 import { OrderStatus } from "@xtptickets/common";
 
+/**
+ * สำคัญ
+ * Because we are mocking out the stripe module right here when we write out this import { stripe } from "../../stripe";
+ * jest going to make sure that we get the stripe file inside of our __mocks__ folder, Not the real version of the stripe file.
+ * 
+ * หมายความว่า Jest จะไม่โหลดโค้ดจริงในไฟล์ stripe.ts หรือ stripe.js แต่จะพยายามหาไฟล์ mock ที่ชื่อเหมือนกันใน __mocks__/stripe.ts(หรือ .js)
+ * เมื่อเรียก import { stripe } แบบนี้และมี jest.mock(...) ตามมา Jest จะมองหาไฟล์ __mocks__/stripe.ts ในโฟลเดอร์เดียวกับไฟล์ทดสอบ แล้วใช้ตัว mock แทนตัวจริงเลย
+ * 
+ * 
+ */
+import { stripe } from "../../stripe";
+jest.mock('../../stripe'); // เป็นคำสั่งของ Jest ที่สั่งให้ "mock" โมดูล ../../stripe
+
+
+
 it("returns a 404 when purchasing an order that does not exist", async () => {
   await request(app)
     .post("/api/payments")
@@ -55,4 +70,31 @@ it('returns a 400 when purchasing a cancelled order', async() => {
       token: 'mock_token'
     })
     .expect(400)
+});
+
+it('returns a 204 with valid inputs', async () => {
+  const userId = new mongoose.Types.ObjectId().toHexString();
+
+  const order = Order.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    userId,
+    version: 0,
+    price: 20,
+    status: OrderStatus.Created,
+  });
+  await order.save();
+
+  await request(app)
+    .post('/api/payments')
+    .set('Cookie', global.signin(userId))
+    .send({
+      token: 'tok_visa',
+      orderId: order.id
+    })
+    .expect(201);
+
+    const chargeOptions = (stripe.charges.create as jest.Mock).mock.calls[0][0];
+    expect(chargeOptions.source).toEqual('tok_visa');
+    expect(chargeOptions.amount).toEqual(20 * 100);
+    expect(chargeOptions.currency).toEqual('thb');
 });
--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -9,6 +9,7 @@ import {
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import { Order } from "../models/order";
+import { stripe } from "../stripe";
 
 const router = express.Router();
 
@@ -39,7 +40,13 @@ router.post(
       throw new BadRequestError("Cannot pay for an cancelled order"); // order expired
     }
 
-    res.send({ success: true });
+    await stripe.charges.create({
+      currency: 'thb',
+      amount: order.price * 100,
+      source: token
+    });
+
+    res.status(201).send({ success: true });
   }
 );
 
